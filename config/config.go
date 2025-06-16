600a696196050885bc2e43c778385337e6a396cd
Kurush Qosimi <kurushqosimi@gmail.com>
2025-06-16 10:42:11 +0500

    add: carcas for application

---
+++ config/config.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"fmt"
+	"github.com/ilyakaznacheev/cleanenv"
+	"time"
+)
+
+type (
+	Config struct {
+		App  `yaml:"app"`
+		HTTP `yaml:"http"`
+		Log  `yaml:"logger"`
+	}
+
+	App struct {
+		Name    string `env-required:"true" yaml:"name" env:"APP_NAME"`
+		Version string `env-required:"true" yaml:"version" env:"APP_VERSION"`
+	}
+
+	HTTP struct {
+		Port         string        `env-required:"true" yaml:"port" env:"HTTP_PORT"`
+		ReadTimeout  time.Duration `env-required:"true" yaml:"read-timeout" env:"HTTP-READ-TIMEOUT"`
+		WriteTimeout time.Duration `env-required:"true" yaml:"write-timeout" env:"HTTP-WRITE-TIMEOUT"`
+		CORS         struct {
+			AllowedMethods     []string `env-required:"true" yaml:"allowed-methods" env:"HTTP-CORS-ALLOWED-METHODS"`
+			AllowedOrigins     []string `env-required:"true" yaml:"allowed-origins"`
+			AllowCredentials   bool     `env-required:"true" yaml:"allow-credentials"`
+			AllowedHeaders     []string `env-required:"true" yaml:"allowed-headers"`
+			OptionsPassthrough bool     `yaml:"options-passthrough"`
+			ExposedHeaders     []string `env-required:"true" yaml:"exposed-headers"`
+			Debug              bool     `env-required:"true" yaml:"debug"`
+		} `yaml:"cors"`
+	}
+
+	Log struct {
+		Level string `env-required:"true" yaml:"log_level" env:"LOG_LEVEL"`
+	}
+)
+
+func New() (*Config, error) {
+	cfg := &Config{}
+
+	err := cleanenv.ReadConfig("./config/config.yaml", cfg)
+	if err != nil {
+		return nil, fmt.Errorf("config error: %w", err)
+	}
+
+	err = cleanenv.ReadEnv(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
---
+++ internal/app/app.go
@@ -0,0 +1,143 @@
+package app
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/rs/cors"
+	"golang.org/x/sync/errgroup"
+	"net"
+	"net/http"
+	"ngMarketplace/config"
+	http2 "ngMarketplace/internal/transport/http"
+	"ngMarketplace/pkg/logger"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+type App struct {
+	cfg        *config.Config
+	wg         sync.WaitGroup
+	router     *gin.Engine
+	httpServer http.Server
+	logger     logger.Logger
+}
+
+func New(cfg *config.Config) (*App, error) {
+	var a = &App{
+		wg: sync.WaitGroup{},
+	}
+
+	l := logger.New(logger.WithLevel(cfg.Log.Level), logger.WithIsJSON(true), logger.WithAddSource(true))
+	l.Debug("logger initialized")
+
+	l.Debug("Configurations: %v", cfg)
+
+	//pg, err := postgres.New(cfg.PG.URL, postgres.MaxPoolSize(cfg.PG.PoolMax))
+	//if err != nil {
+	//	l.Fatal(fmt.Sprintf("app - Run - postgres.New: %v", err))
+	//}
+	//l.Debug("PostgreSQL initialized")
+
+	//runner := async.NewBackgroundRunner(&a.wg)
+
+	router := http2.NewRouter()
+
+	a.cfg = cfg
+	a.router = router
+	a.logger = l
+
+	return a, nil
+}
+
+func (a *App) Run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	grp, ctx := errgroup.WithContext(ctx)
+
+	grp.Go(func() error {
+		return a.startHTTP(ctx)
+	})
+
+	err := grp.Wait()
+	switch {
+	case err == nil || errors.Is(err, context.Canceled):
+		a.logger.Info("Everything is good. Server shutting down...")
+	case errors.Is(err, http.ErrServerClosed):
+		a.logger.Info("server shutdown")
+	default:
+		a.logger.Error(fmt.Sprintf("app.Run: %v", err))
+		return err
+	}
+
+	a.logger.Info("waiting for background tasks to finish...")
+	a.wg.Wait()
+
+	//if a.pg != nil {
+	//	a.pg.Close()
+	//	a.logger.Info("Postgres connection closed")
+	//}
+
+	return nil
+}
+
+func (a *App) startHTTP(ctx context.Context) error {
+	const op = "startHTTP"
+
+	a.logger.Info("HTTP Server initializing")
+
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%s", "0.0.0.0", a.cfg.HTTP.Port))
+	if err != nil {
+		a.logger.Fatal("%s: failed to create listener: ", op, err)
+		return err
+	}
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:     a.cfg.HTTP.CORS.AllowedOrigins,
+		AllowedMethods:     a.cfg.HTTP.CORS.AllowedMethods,
+		AllowedHeaders:     a.cfg.HTTP.CORS.AllowedHeaders,
+		ExposedHeaders:     a.cfg.HTTP.CORS.ExposedHeaders,
+		AllowCredentials:   a.cfg.HTTP.CORS.AllowCredentials,
+		OptionsPassthrough: a.cfg.HTTP.CORS.OptionsPassthrough,
+		Debug:              a.cfg.HTTP.CORS.Debug,
+		Logger:             a.logger,
+	})
+
+	handler := c.Handler(a.router)
+
+	a.httpServer = http.Server{
+		Handler:      handler,
+		WriteTimeout: a.cfg.HTTP.WriteTimeout,
+		ReadTimeout:  a.cfg.HTTP.ReadTimeout,
+	}
+
+	go func() {
+		<-ctx.Done()
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		a.logger.Info("Shutting down HTTP server...")
+		err = a.httpServer.Shutdown(shutdownCtx)
+		if err != nil {
+			a.logger.Error("failed to shutdown server: %v", err)
+		}
+	}()
+
+	if err = a.httpServer.Serve(listener); err != nil {
+		switch {
+		case err == nil:
+			a.logger.Info("server exited with no error")
+			return nil
+		case errors.Is(err, http.ErrServerClosed):
+			a.logger.Info("server shutdown")
+		default:
+			a.logger.Fatal("failed to start server")
+		}
+	}
+
+	return err
+}
